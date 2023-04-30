89ba3171768120dcbb7d19724385943eb603a7fe
Jeff Schwab <jeff@unbuggy.com>
2023-04-29 22:48:42 -0400

    Fix a typo in a comment

--- chapter2/src/bin/factorial-by-recursion.rs
+++ chapter2/src/bin/factorial-by-recursion.rs
@@ -1,5 +1,5 @@
 fn factorial(number: u32) -> u32 {
-    // The original example uses 1, rather than 0, as the base case, causing a
+    // The original example uses 1, rather than 0, as the base case, causing
     // factorial(0) to never return.  That bug is fixed here.
     match number {
         0 => 1,                              // BASE CASE
